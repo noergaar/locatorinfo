f05238ecd8dcc6de24ca8eede419aef824eb06e6
Bjarne Nørgaard <bjarne.norgaard@nokia.com>
2011-07-18 10:25:22 +0200

    Intial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Locatorinfo::Application.config.secret_token = '91ea89279f16d1848e964784092f32af0c5e7c7bb5e1a097390d80e84cee8958cf441572df942f56673b7c4ac21fdecf6d4b925f8a199d69b51733d87a989875'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Locatorinfo::Application.config.session_store :cookie_store, :key => '_locatorinfo_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Locatorinfo::Application.config.session_store :active_record_store
