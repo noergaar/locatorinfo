ee16c41c5b2a31ae26cf80b4be7907d092c8b2ca
Bjarne Nørgaard <bjarne.norgaard@nokia.com>
2011-07-18 10:32:36 +0200

    set root index

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Locatorinfo::Application.routes.draw do
+
+  get "locations/index"
   resources :locations
 
   # The priority is based upon order of creation:
@@ -50,7 +52,7 @@ Locatorinfo::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => "welcome#index"
+ root :to => "locations#index"
 
   # See how all your routes lay out with "rake routes"
 
